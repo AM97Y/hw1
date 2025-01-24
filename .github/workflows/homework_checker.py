1a0dbdbc0476dc66ade76f22da51da1afa9be374
AM <43960358+AM97Y@users.noreply.github.com>
2025-01-24 19:34:27 +0300

    Update homework_checker.py

--- .github/workflows/homework_checker.py
+++ .github/workflows/homework_checker.py
@@ -1,7 +1,21 @@
-print("Проверка запущена. Результаты будут в комментариях к Pull Request.")
-
-# Пример: просто выводим сообщение в stdout
 import time
+import os
+import subprocess
+# Здесь должен быть ваш реальный код проверки
+# 1. Конвертация
+# например: jupyter nbconvert --to python <файл>.ipynb
+# 2. Разделение
+# 3. Фидбек
+# 4. Советы
+def get_result_string(feedback_list):
+   return "\n".join([f"Блок: {index}, Отзыв: {feedback}" for index, feedback in enumerate(feedback_list)])
+
+# Пример вызова
+feedback_list = ["Отлично", "Хорошо", "Есть недочеты"]
+
+result_string = get_result_string(feedback_list)
+
 time.sleep(5) # Задержка для демонстрации
-print("ДЗ прошло проверку.")
-print("Совет: Ура!.")
+print("Проверка запущена. Результаты будут в комментариях к Pull Request.")
+print(f"Результаты:\n {result_string}")
+print("Совет: Подумайте над улучшением.")
